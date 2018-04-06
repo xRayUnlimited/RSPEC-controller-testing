cc7faa6f8ba7422a6b6959acf5dea133de613bec
Ray Suyoto <ray.suy@icloud.com>
2018-04-06 10:56:48 -0600

    add testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name, :gender, :age
   validates_inclusion_of :gender, in: %w(Male Female Other)
-  validates :age, numericality: {
-    greater_than_or_equal_to: 0,
-    less_than_or_equal_to: 120
+  validates :age, numericality: { 
+    greater_than_or_equal_to: 0, 
+    less_than_or_equal_to: 120 
   }
 
   has_many :bank_accounts, dependent: :destroy
--- spec/controllers/bank_accounts_controller_spec.rb
+++ spec/controllers/bank_accounts_controller_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe BankAccountsController, type: :controller do
+  login_user
 
+  let(:valid_attributes) {
+    { institution: 'Chase', amount: 10000, active: true }
+  }
+  let(:invalid_attributes) {
+    { institution: '', amount: 200, active: true }
+  }
+
+  describe "GET #index" do
+    it "returns a success response" do
+      bank_account = @user.bank_accounts.create! valid_attributes
+      get :index
+      expect(response).to be_success
+    end
+  end
+  
+  describe "GET #show" do
+    it "returns a success response" do
+      bank_account = @user.bank_accounts.create! valid_attributes
+      get :show, params: {id: bank_account.id}
+      expect(response).to be_success
+    end
+  end
+
+  describe "GET #new" do
+    it "returns a success response" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+  
+  describe "GET #edit" do
+    it "returns a success response" do
+      bank_account = @user.bank_accounts.create! valid_attributes
+      get :edit, params: {id: bank_account.id}
+      expect(response).to be_success
+    end
+  end
+
+
+  describe "POST #create" do
+    context "with valid params" do
+      
+    end
+    context "with invalid params" do
+
+    end
 end
